996a24ceb6c04c438455a033fb2b958343be560a
Giuseppe Abrami <abrami@em.uni-frankfurt.de>
2022-08-05 09:22:14 +0200

    CAS add fs2JSON

--- src/main/java/org/texttechnologylab/utilities/uima/jcas/JCasTTLabUtils.java
+++ src/main/java/org/texttechnologylab/utilities/uima/jcas/JCasTTLabUtils.java
@@ -3,14 +3,19 @@ package org.texttechnologylab.utilities.uima.jcas;
 import de.tudarmstadt.ukp.dkpro.core.api.segmentation.type.Sentence;
 import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_engine.AnalysisEngineProcessException;
+import org.apache.uima.cas.Feature;
 import org.apache.uima.fit.component.JCasAnnotator_ImplBase;
 import org.apache.uima.fit.factory.JCasFactory;
 import org.apache.uima.fit.util.JCasUtil;
 import org.apache.uima.jcas.JCas;
+import org.apache.uima.jcas.cas.FSArray;
 import org.apache.uima.jcas.cas.TOP;
 import org.apache.uima.jcas.tcas.Annotation;
 import org.apache.uima.resource.ResourceInitializationException;
 import org.apache.uima.util.CasCopier;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.lang.reflect.InvocationTargetException;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -181,4 +186,75 @@ public class JCasTTLabUtils {
     }
 
 
+    public static JSONObject fs2JSON(TOP fs, boolean bDetail) throws JSONException {
+        JSONObject fsfObj = new JSONObject();
+        for (Feature feature : fs.getType().getFeatures()) {
+            if (feature.getName().equals("uima.cas.AnnotationBase:sofa")) {
+                continue;
+            }
+            switch (feature.getRange().getName()) {
+                case "uima.cas.String": {
+                    fsfObj.put(feature.getShortName(), fs.getStringValue(feature));
+                    break;
+                }
+                case "uima.cas.Integer": {
+                    fsfObj.put(feature.getShortName(), fs.getIntValue(feature));
+                    break;
+                }
+                case "uima.cas.Float": {
+                    fsfObj.put(feature.getShortName(), fs.getFloatValue(feature));
+                    break;
+                }
+                case "uima.cas.Long": {
+                    fsfObj.put(feature.getShortName(), fs.getLongValue(feature));
+                    break;
+                }
+                case "uima.cas.Boolean": {
+                    fsfObj.put(feature.getShortName(), fs.getBooleanValue(feature));
+                    break;
+                }
+                case "uima.cas.Short": {
+                    fsfObj.put(feature.getShortName(), fs.getShortValue(feature));
+                    break;
+                }
+                case "uima.cas.Double": {
+                    fsfObj.put(feature.getShortName(), fs.getDoubleValue(feature));
+                    break;
+                }
+                case "uima.cas.Byte": {
+                    fsfObj.put(feature.getShortName(), fs.getByteValue(feature));
+                    break;
+                }
+                default: {
+                    TOP fSF = (TOP) fs.getFeatureValue(feature);
+                    if (fSF != null) {
+                        if (fSF.getType().getName().equals("uima.cas.FSArray")) {
+                            JSONArray jsonArray = new JSONArray();
+                            FSArray array = (FSArray) fSF;
+                            for (int i = 0; i < array.size(); i++) {
+                                TOP arrayElement = (TOP) array.get(i);
+                                jsonArray.put(arrayElement.getAddress());
+                            }
+                            fsfObj.put(feature.getShortName(), jsonArray);
+                        } else {
+                            if (bDetail) {
+                                fsfObj.put(feature.getShortName(), fs2JSON(fSF, bDetail));
+                            } else {
+                                fsfObj.put(feature.getShortName(), fSF.getAddress());
+                            }
+
+                        }
+                    } else {
+                        fsfObj.put(feature.getShortName(), "null");
+                    }
+                }
+            }
+        }
+
+        JSONObject fsObj = new JSONObject();
+        fsObj.put("_addr", fs.getAddress());
+        fsObj.put("_type", fs.getType().getName());
+        fsObj.put("features", fsfObj);
+        return fsObj;
+    }
 }
